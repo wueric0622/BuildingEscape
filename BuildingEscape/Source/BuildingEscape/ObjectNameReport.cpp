9f9d85810421beb27bbe61dc941687bedd2ab966
吳孟紘 <wueric0622@gmail.com>
2017-08-01 18:31:22 +0800

    Get Object Name

--- BuildingEscape/Source/BuildingEscape/ObjectNameReport.cpp
+++ BuildingEscape/Source/BuildingEscape/ObjectNameReport.cpp
@@ -1,33 +1,30 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PositionRepot.h"
-#include "GameFramework/Actor.h"
+#include "ObjectNameReport.h"
 
 
 // Sets default values for this component's properties
-UPositionRepot::UPositionRepot()
+UObjectNameReport::UObjectNameReport()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	
+	// ...
 }
 
 
 // Called when the game starts
-void UPositionRepot::BeginPlay()
+void UObjectNameReport::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	FString ObjectName = GetOwner()->GetName();
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for %s"), *ObjectName);
+	// ...
 	
 }
 
 
 // Called every frame
-void UPositionRepot::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UObjectNameReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
